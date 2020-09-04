a8373cd183f934e9c1b8c560ff0ef2715b529504
gliitterpuke <katherinewwang@gmail.com>
2020-09-03 23:45:30 -0400

    test: login role redirect

--- src/app/services/jwtAuthService.js
+++ src/app/services/jwtAuthService.js
@@ -44,6 +44,18 @@ class JwtAuthService {
       this.setUser(response.data.data);
       console.log(localStorageService.getItem("auth_user"))
     })
+    .then(() => { 
+      let newuser = localStorageService.getItem("auth_user")
+      if (newuser.role === "client") {
+      history.push({
+        pathname: "/profile"
+      })
+      } else if (newuser.role === "professional") {
+      history.push({
+        pathname: "/professional"
+      })
+      }
+    })
   };
   
   // Save user to localstorage
